4ceb8f46c273de1cc58a458781f945a7f8998303
pramodliv1 <pramodliv1@gmail.com>
2013-11-29 14:51:43 +0530

    Added topo admin

--- server/cg/topo/admin.py
+++ server/cg/topo/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Topic, Concept, ConceptRelationship, ConceptResource
+
+class ConceptResourceInline(admin.StackedInline):
+	model = ConceptResource
+	extra = 1
+
+
+admin.site.register(Topic)
+admin.site.register(ConceptRelationship)
+
+class ConceptAdmin(admin.ModelAdmin):
+	inlines = [ConceptResourceInline]
+	prepopulated_fields = {"slug": ("name",)}
+
+
+
+
+admin.site.register(Concept, ConceptAdmin)
+
+
