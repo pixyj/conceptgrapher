18ab544e28f40c845e3023cea39e38c9eebc131e
pramodliv1 <pramodliv1@gmail.com>
2013-12-16 23:56:40 +0530

    1. Added management command to backup all tables except attempts 2. More quizzes added

---
+++ server/cg/uber/management/commands/backup.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand, CommandError
+from uber import data
+
+class Command(BaseCommand):
+	help = "Backup sqlite3 data, clear sqlite3.db + redis keys"
+
+	def handle(self, *args, **kwargs):
+		data.dumpdata()
+		self.stdout.write("Done")
+
+
+
+
