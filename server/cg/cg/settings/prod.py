f518652af8e4ee572d163ed5e683583427e472ae
pramodliv1 <pramodliv1@gmail.com>
2014-01-01 13:25:41 +0530

    Added psycopg2 to requirements

--- server/cg/cg/settings/prod.py
+++ server/cg/cg/settings/prod.py
@@ -1,10 +1,23 @@
 from .base import *
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
 
 COMPRESS_ENABLED = True
 
 COMPRESS_OFFLINE = True
+
+PG_PASSWORD = os.environ["PG_PASSWORD"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cgdb',                     
+        'USER': 'postgres',
+        'PASSWORD': PG_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
