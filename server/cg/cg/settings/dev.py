8a4483ac9cea3720e98db8edc17a3968b6d8786e
pramodliv1 <pramodliv1@gmail.com>
2014-01-01 14:10:35 +0530

    Create dev settings

---
+++ server/cg/cg/settings/dev.py
@@ -0,0 +1,10 @@
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'sqlite3.db'),
+    }
+}
+
