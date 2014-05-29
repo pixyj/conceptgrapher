4f12a2e5cb9f766a18de8be9492f993f98064ec4
pramodliv1 <pramodliv1@gmail.com>
2014-05-29 23:08:56 +0530

    More links to concepts from topic page

--- client/js/quiz.js
+++ client/js/quiz.js
@@ -154,6 +154,7 @@ var ChoiceCollection = Backbone.Collection.extend({
 });
 
 var Attempt = Backbone.Model.extend({
+	idAttribute: "guess",
 	parse: function(attrs) {
 		if(!attrs.created) {
 			attrs.created = new Date();
